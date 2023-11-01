aedf08bc39f762d55d187423ae8fae831d1b26be
Chuk <chukmunnlee@gmail.com>
2023-11-01 16:17:04 +0800

    day04 PM demos

--- day04/src/sdf/day04/CountWordsFromFile.java
+++ day04/src/sdf/day04/CountWordsFromFile.java
@@ -11,12 +11,22 @@ public class CountWordsFromFile {
       BufferedReader br = new BufferedReader(fr);
 
       String line;
+      int count = 0;
       while ((line = br.readLine()) != null) {
-         System.out.printf("> line: %s\n", line);
+         // Clean the line
+         line = line.trim().replaceAll("[^\\sa-zA-Z0-9]", "");
+         // Check if it is line is empty
+         if (line.length() <= 0)
+            continue;
+         // Split the line into words delimited by single space
+         String[] words = line.split(" ");
+         // Count the number of words and add to total
+         count += words.length;
       }
 
       br.close();
-      
+
+      System.out.printf("Number of words in %s is %d\n", args[0], count);
    }
    
 }
---
+++ day04/src/sdf/day04/CountWordsWithStream.java
@@ -0,0 +1,39 @@
+package sdf.day04;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Optional;
+
+public class CountWordsWithStream {
+
+   public static void main(String[] args) throws Exception {
+      // Open the file
+      try (FileReader fr = new FileReader(args[0])) {
+         BufferedReader br = new BufferedReader(fr);
+
+         // What is this Optional?
+         Optional<Integer> opt = br.lines()
+            // Clean the line
+            .map(line -> line.trim().replaceAll("[^\\sa-zA-Z0-9]", ""))
+            // Remove empty lines
+            .filter(line -> line.length() > 0)
+            // line -> words 
+            .map(line -> line.split(" "))
+            // words -> count
+            .map(words -> words.length)
+            .reduce((acc, x) -> acc + x);
+
+         int count = 0;
+
+         if (opt.isPresent()) {
+            count = opt.get();
+            System.out.printf("Number of words in %s is %d\n", args[0], count);
+         } else {
+            System.out.println("No result");
+         }
+
+
+      }
+   }
+   
+}
