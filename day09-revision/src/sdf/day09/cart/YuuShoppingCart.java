b3bb9167f6c4b4e5043ec57f7e47fd7975a5e283
Chuk <chukmunnlee@gmail.com>
2023-11-09 16:14:59 +0800

    day09 update @Override example

---
+++ day09-revision/src/sdf/day09/cart/YuuShoppingCart.java
@@ -0,0 +1,14 @@
+package sdf.day09.cart;
+
+import java.util.List;
+
+public class YuuShoppingCart extends ShoppingCart {
+
+	public YuuShoppingCart(String name) {
+		super(name);
+	}
+
+	// My INTENTION is to override the load() from ShoppingCart
+   public void load(String name) {
+	}
+}
