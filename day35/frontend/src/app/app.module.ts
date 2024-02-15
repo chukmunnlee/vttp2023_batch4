9715aadfbb5c6c4d3b32c1696e65dac36c9c60ff
Chuk <chukmunnlee@gmail.com>
2024-02-15 10:23:12 +0800

    day35 AM demos

--- day35/frontend/src/app/app.module.ts
+++ day35/frontend/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ReactiveFormsModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
