0642f9ff9d6cdf6d3d47450d8beab2249e12a79c
chukmunnlee <chukmunnlee@gmail.com>
2024-02-15 08:36:47 +0800

    day35 template

---
+++ day35/backend/src/test/java/vttp2024/batch4/csf/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2024.batch4.csf.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
