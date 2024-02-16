c48abb853c4e28a3dc2a863e4a15eb0b8088e650
chukmunnlee <chukmunnlee@gmail.com>
2024-02-16 17:03:19 +0800

    day36 werewolf template

---
+++ day36-werewolf/werewolf/src/main/java/vttp/batch4/csf/werewolf/WerewolfApplication.java
@@ -0,0 +1,13 @@
+package vttp.batch4.csf.werewolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerewolfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WerewolfApplication.class, args);
+	}
+
+}
---
+++ day36-werewolf/werewolf/src/test/java/vttp/batch4/csf/werewolf/WerewolfApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.batch4.csf.werewolf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WerewolfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
